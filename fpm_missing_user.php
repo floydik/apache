f4835eca5997308009c64f0544c8b6265e0c599e
Zbynek Vyboh <zbynek@vyboh.net>
2021-01-29 17:45:26 +0100

    Create fpm_missing_user.php

---
+++ fpm_missing_user.php
@@ -0,0 +1,14 @@
+<?php
+/* Description:
+   Errors are in /var/log/syslog 
+   prefix: php-fpm
+   find wrong config file and delete or move them
+   log action
+   restart php-fpm
+*/
+
+// $re = '/php-fpm(\d\.\d).*ERROR.*pool\s(site\d+)/m';
+//preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
+//var_dump($matches);
+
+?>
