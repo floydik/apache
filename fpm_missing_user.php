181fa56e7a608e0c8e299c6c65fd0231ce0de325
Zbynek Vyboh <zbynek@vyboh.net>
2021-01-29 17:58:51 +0100

    Update fpm_missing_user.php

--- fpm_missing_user.php
+++ fpm_missing_user.php
@@ -11,4 +11,18 @@
 //preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
 //var_dump($matches);
 
+// settings
+$logfile = "/home/vyboh/fpm_missing_user.log";
+$fpmpath = "/etc/php/";
+$fpmpool = "fpm/pool.d/";
+$syslog = "/var/log/syslog";
+
+// log utility
+// format: date time: event 
+$line = "";
+$fl = fopen($logfile,"a+"); //write at end of file
+fwrite($fl, $line);
+fclose($fl);
+
+
 ?>
